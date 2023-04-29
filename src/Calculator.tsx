240ceaf6e20c303d5c54a5695835e0646ee75c0b
Eve Stroud <evestroud89@gmail.com>
2023-04-29 07:33:26 -0400

    Add style for calculator body

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -8,16 +8,21 @@ import { Box, Container, Flex } from "@chakra-ui/react";
 const Calculator = () => {
   const { input, output, second, insert, keyHandler } = useCalculator();
   return (
-    <Flex
-      flexDir="column"
-      justifyContent="center"
-      alignItems="center"
-      h="100vh"
-    >
-      <InputScreen input={input} second={second} insert={insert} />
-      <OutputScreen output={output} />
-      <Box id="logo">TS-30</Box>
-      <Controls controls={controls} second={second} keyHandler={keyHandler} />
+    <Flex justifyContent="center" alignItems="center" h="100vh" w="100vw">
+      <Flex
+        flexDir="column"
+        bg="grey"
+        gap="2"
+        p="2"
+        border="2px solid black"
+        borderRadius="5px"
+        boxShadow="5px 5px 5px grey"
+      >
+        <InputScreen input={input} second={second} insert={insert} />
+        <OutputScreen output={output} />
+        <Box id="logo">TS-30</Box>
+        <Controls controls={controls} second={second} keyHandler={keyHandler} />
+      </Flex>
     </Flex>
   );
 };
